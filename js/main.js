41a39e90e9ede143c739c957890925f7f1d16e45
web-intelligent <wwi19878@mail.ru>
2020-09-18 11:49:55 +0600

    Меню готово без стилей

--- js/main.js
+++ js/main.js
@@ -14,14 +14,4 @@ $(document).ready(function () {
         $(this).next().slideToggle();
         
     });
-
-    // $('.menu li a').click(function () {
-    //     if (!$(this).hasClass('active')) {	//если "кликнутый" пункт неактивный:
-    //          $('ul.akkordeon1 li > p').removeClass('active').next('div').slideUp(); //делаем неактивными все пункты и скрываем все блоки
-    //         $(this).addClass('active');	//активируем "кликнутый" пункт
-    //         $(this).next('div').slideDown(200);	//раскрываем следующий за ним блок с описанием
-    //     } else {	//иначе:
-    //         $(this).removeClass('active').next('div').slideUp();	//скрываем данный пункт
-    //     }
-    // });
 });
