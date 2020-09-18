4737f166a30386e9acd7bda150a2cff2833966cc
web-intelligent <wwi19878@mail.ru>
2020-09-18 16:56:36 +0600

    Новая версия

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
 $(document).ready(function () {
     $('.menu ul').slideUp();
 
-    $('.menu li a').click(function (e) { 
-        e.preventDefault();
-        
-        
-        if (!$(this).next().is(':visible')) {
-            // Если кликнутый элемент не виден
-            $('.menu ul').slideUp();
-            $(this).closest('ul').slideDown();
+
+
+    $('.menu li a').click(function () { 
+
+        var menu = $(this).closest('.menu');
+
+        // Если кликнутый элемент не виден
+        if(!$(this).next().is(':visible')) {
+            menu.find('ul').slideUp();
+            menu.find('li').removeClass('active');
+            menu.find('a').removeClass('active_link');
         }
 
         $(this).next().slideToggle();
+        $(this).parent().toggleClass('active');
+        $(this).toggleClass('active_link');
         
     });
 });
